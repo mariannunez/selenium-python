7b2ea116b115373fd43aa8dddd6b8b3a9aea7534
mariana280721 <mariana.nunez@gfe.frre.utn.edu.ar>
2023-04-11 12:59:52 -0300

    add --headless for ci executions

--- utils/base_driver.py
+++ utils/base_driver.py
@@ -4,15 +4,17 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
-options = Options()
+import os
 
 
 @pytest.fixture()
 def create_driver(request):
     opts = Options()
-    options.page_load_strategy = 'normal'
+    if os.environ.get('CI_EXECUTION', False):
+        opts.add_argument('--headless')
+    opts.page_load_strategy = 'normal'
     chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
-    request.cls.driver = webdriver.Chrome(service=chrome_service, options=options)
+    request.cls.driver = webdriver.Chrome(service=chrome_service, options=opts)
     request.cls.driver.maximize_window()
 
 
