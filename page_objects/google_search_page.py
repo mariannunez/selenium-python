22387ca3118367831b4d7f9ae2a45b57faf1b628
mariana280721 <mariana.nunez@gfe.frre.utn.edu.ar>
2023-04-11 15:06:04 -0300

    Workaround for CI execution

--- page_objects/google_search_page.py
+++ page_objects/google_search_page.py
@@ -20,9 +20,11 @@ class GoogleSearchPage(BasePage):
         self.enter_text(By.NAME, "q", value)
         self.click(By.CSS_SELECTOR, "[role='listbox'] li")
 
-    def get_temp(self):
+    def get_temp(self, expected_unit='°Celsius'):
         temp = self.get_element_text(By.ID, "wob_tm")
-        unit = self.get_attribute_value(
-            By.XPATH, "//div[@class='vk_bk wob-unit']/a[contains(@style,'display:none')]/span", "aria-label"
-        )
+        unit_button = "//div[@class='vk_bk wob-unit']/a[contains(@style,'display:none')]"
+        unit = self.get_attribute_value(By.XPATH, f'{unit_button}/span', "aria-label")
+        if unit != expected_unit:
+            self.click(By.XPATH, unit_button)
+        unit = self.get_attribute_value(By.XPATH, f'{unit_button}/span', "aria-label")
         return WeatherData(temp, unit)
